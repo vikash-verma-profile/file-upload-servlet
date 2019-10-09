774cee897021e5a36a2a149ae6166e20c5036566
ervikashverma <er.vikashverma@yahoo.com>
2019-10-09 11:53:54 +0530

    first commit

---
+++ src/java/UploadServlet.java
@@ -0,0 +1,22 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.oreilly.servlet.MultipartRequest;   
+
+public class UploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        MultipartRequest m = new MultipartRequest(request, "C:/uploads");
+        out.print("successfully uploaded");
+    }
+}
